22d1cfb371871e2a430308fc40a647b25ee00e61
webdeeva <tavonia@gmail.com>
2024-01-30 14:25:38 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,11 @@ export default function Home() {
     <main className="main">
       <div className="container">
         <div className="header">
-          <p>
-            Signless + gasless UX using{" "}
-            <a href="https://portal.thirdweb.com/wallet/local-wallet">
-              LocalWallet
-            </a>{" "}
-            and{" "}
-            <a href="https://portal.thirdweb.com/wallet/smart-wallet">
-              SmartWallet
-            </a>
-          </p>
-          <hr className="divider" />
-          <p className="description">
-            With <code className="code">{"<ConnectWallet />"}</code> UI
-            component{" "}
-          </p>
           <div className="connect">
             <ConnectWallet btnTitle="Connect" />
           </div>
         </div>
-        <div className="connect">
-          <hr className="divider" />
-          <p className="description">
-            With <code className="code">{"useConnect()"}</code> hook and custom
-            UI
-          </p>
-        </div>
+       
         <ConnectUI />
        
         <NFTComponent />
